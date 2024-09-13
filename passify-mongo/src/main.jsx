49d1886e5865af4a521873958eb2bb71a56f0f48
PrathamSahu7777 <139504189+PrathamSahu7777@users.noreply.github.com>
2024-09-13 20:43:15 +0530

    main.jsx

--- passify-mongo/src/main.jsx
+++ passify-mongo/src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
-    <App />
+  <Auth0Provider
+    domain="dev-aixmgq1ovdqj6y8m.us.auth0.com"
+    clientId="520OcMTqqhqP8TyQHR4WQOsiG41MY3VD"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
+     <App />
+  </Auth0Provider>
+   
   // </StrictMode>,
 )
