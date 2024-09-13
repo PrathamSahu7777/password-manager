94e0c5aa493f09acab878e79c07bb508a8ea6018
PrathamSahu7777 <139504189+PrathamSahu7777@users.noreply.github.com>
2024-09-13 20:40:35 +0530

    Navbar.jsx

--- passify-mongo/src/components/Navbar.jsx
+++ passify-mongo/src/components/Navbar.jsx
@@ -1,17 +1,29 @@
 import React from 'react'
+import { useAuth0 } from "@auth0/auth0-react";
+
 
 const Navbar = () => {
+
+    const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
+
     return (
         <nav className='flex justify-between bg-slate-300 px-1 md:px-36 h-10 items-center '>
             <div className="logo font-bold text-2xl">Pass'
                 <span className='text-green-500'>ify</span>
-                </div>
-            <ul>
+            </div>
+            <ul className='flex gap-4'>
                 <li className='flex gap-4 '>
                     <a href="/" className='hover:font-bold'>Home</a>
                     <a href="#" className='hover:font-bold'>About</a>
                     <a href="#" className='hover:font-bold'>Contact</a>
                 </li>
+                {
+                    isAuthenticated ? <li> <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+                        Log Out
+                    </button></li> : <li><button onClick={() => loginWithRedirect()}>Log In</button></li>
+                }
+
+
             </ul>
         </nav>
     )
